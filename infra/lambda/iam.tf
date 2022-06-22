147d5f4201754ddcb9d7f8b2ebe1abe09e967947
kryptn <kryptn@gmail.com>
2022-06-21 21:56:13 -0600

    remove ssm read policy for now

--- infra/lambda/iam.tf
+++ infra/lambda/iam.tf
@@ -14,35 +14,42 @@ resource "aws_iam_role" "exec" {
     ]
   })
 
-  managed_policy_arns = [ aws_iam_policy.ssm_read.arn]
 }
 
-resource "aws_iam_policy" "ssm_read" {
-    name = "${var.name}-ssm-read"
-    policy = jsonencode({
-            Version = "2012-10-17"
-    Statement = [
-      {
-        Action   =[
-              "ssm:GetParameter",
-              "ssm:GetParameters",
-              "ssm:GetParametersByPath",
-            ]
-        Effect   = "Allow"
-        Resource = "arn:aws:ssm:::parameter/websub/"
-      },
-    ]
-    })
+# resource "aws_iam_policy" "ssm_read" {
+#     name = "${var.name}-ssm-read"
+#     policy = jsonencode({
+#             Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action   =[
+#               "ssm:GetParameter",
+#               "ssm:GetParameters",
+#               "ssm:GetParametersByPath",
+#             ]
+#         Effect   = "Allow"
+#         Resource = "arn:aws:ssm:::parameter/websub/"
+#       },
+#     ]
+#     })
 
+# }
+
+data "aws_iam_policy" "basic_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "basic_policy" {
   role       = aws_iam_role.exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = data.aws_iam_policy.basic_policy.arn
+}
+
+data "aws_iam_policy" "dynamodb_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_policy" {
   role       = aws_iam_role.exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
+  policy_arn = data.aws_iam_policy.dynamodb_policy.arn
 }
 
